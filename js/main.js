7f9b88c7a2a9613571a71e9e3020f33d6e4c9d7a
oljuanfe <ojuanferrero@gmail.com>
2018-11-01 15:11:11 +0100

    Add fetch and localStorage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+let data;
+const URL = 'https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-setas.json';
+
+
+
+data = JSON.parse(localStorage.getItem('recipe'));
+console.log('data after get', data);
+
+if (data === null) {
+  console.log('entrando if');
+  fetch(URL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      console.log(json);
+      localStorage.setItem('recipe', JSON.stringify(json));
+      return (data = json, console.log('data', data));
+    });
+}
+
+
+
+console.log('data final', data);
