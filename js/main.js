99d219312c8cbbb9cbc6765e9376f41e70a6a306
oljuanfe <ojuanferrero@gmail.com>
2018-11-03 10:37:45 +0100

    Create a dynamic summary list given a dynamic object

--- js/main.js
+++ js/main.js
@@ -10,11 +10,21 @@ const onButton = document.querySelector('.selected-button');
 const offButton = document.querySelector('.deselected-button');
 const mainTitle = document.querySelector('.header-title');
 const ingredientsList = document.querySelector('.ingredients-list');
+const summaryList = document.querySelector('.summary-list');
 
+let summaryPrices = {
+  Items: 0,
+  Subtotal: 0,
+  'Gastos de envio': undefined,
+  Total: 0
+};
 
+// Recupero datos de LocalStorage si los hubiese
 data = JSON.parse(localStorage.getItem('recipe'));
 console.log('data after get', data);
 
+
+// Si en el localStorage no hubiese nada sería data = null, hago llamada a la API
 if (data === null) {
   console.log('entrando if');
   fetch(URL)
@@ -35,28 +45,29 @@ if (data !== null) {
   console.log('ingredients', recipeIngredients);
 }
 
+
+// Añadir título principal a la página
 function addTitle() {
   const recipeTitle = data.recipe.name;
   let newTitleContent = document.createTextNode(recipeTitle);
   mainTitle.appendChild(newTitleContent);
 }
 
+// Creo la lista de todos los ingredientes de la receta, primera sección
 function addIngredientsList() {
   const recipeIngredients = data.recipe.ingredients;
   console.log('allIr', recipeIngredients);
   for (const ingredient of recipeIngredients) {
     console.log('ingre', ingredient);
     let newListItem = document.createElement('li');
-    newListItem.append(createCheckbox(ingredient), addIngredientQuantity(ingredient), addIngredientInfo(ingredient));
+    newListItem.append(createCheckbox(ingredient), addIngredientNumberItems(ingredient), addIngredientInfo(ingredient), addIngredientPrice(ingredient));
     console.log('list', newListItem);
     ingredientsList.appendChild(newListItem);
     console.log('list completed', ingredientsList);
   }
-
-
-
 }
 
+// Creo input checkbox con su label
 function createCheckbox(ingredient) {
   const ingredientName = ingredient.product;
   const newCheckboxLabel = document.createElement('label');
@@ -68,7 +79,8 @@ function createCheckbox(ingredient) {
   return newCheckboxLabel.appendChild(newCheckbox);
 }
 
-function addIngredientQuantity(ingredient) {
+// Añado el número de items del ingrediente
+function addIngredientNumberItems(ingredient) {
   const ingredientItem = ingredient.items;
   const newIngredientItem = document.createElement('div');
   const newItemContent = document.createTextNode(ingredientItem);
@@ -76,6 +88,8 @@ function addIngredientQuantity(ingredient) {
   return newIngredientItem;
 }
 
+
+// Añado la info del ingrediente: nombre, marca, cantidad
 function addIngredientInfo(ingredient) {
   const ingredientInfo = ingredient;
   const newInfoWrapper = document.createElement('div');
@@ -96,11 +110,43 @@ function addIngredientInfo(ingredient) {
   return newInfoWrapper;
 }
 
+// Añado precio del ingrediente
+function addIngredientPrice(ingredient) {
+  const ingredientPrice = ingredient.price;
+  const currency = data.recipe.currency;
+  const newPrice = document.createElement('div');
+  const newPriceContent = document.createTextNode(ingredientPrice + ' ' + currency);
+  newPrice.appendChild(newPriceContent);
+  return newPrice;
+}
+
+// Creo lista resumen precio productos
+
+
+
+// Creo los items de la lista resumen
+function addSummaryList() {
+  for (const itemList in summaryPrices) {
+    let listContent = itemList + ': ' + summaryPrices[itemList] + ' ' + data.recipe.currency;
+    console.log(listContent);
+    const newItemList = document.createElement('li');
+    const newItemListContent = document.createTextNode(listContent);
+    newItemList.appendChild(newItemListContent);
+    summaryList.appendChild(newItemList);
+  }
+  console.log('summary final', summaryList);
+  
+}
+
 addTitle();
-// createCheckbox();
-// addIngredientQuantity();
-// addIngredientInfo();
 addIngredientsList();
+
+summaryPrices['Gastos de envio'] = data.recipe['shipping-cost'];
+summaryPrices['Total'] = summaryPrices['Items'] + summaryPrices['Subtotal'] + summaryPrices['Gastos de envio'];
+console.log('resumen', summaryPrices);
+
+addSummaryList();
+
 
 function selectedAll() {
   console.log('click seleccionar');
